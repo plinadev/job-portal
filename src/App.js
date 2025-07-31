42a6114a0bfceb4c56dc9c1a31fbc82e9c9490d0
plinadev <pavlenkopolina415@gmail.com>
2025-07-31 13:39:15 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div className="m-5">
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
+}
+
+export default App;
